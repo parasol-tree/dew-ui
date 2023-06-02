1a3a172b57845c190971e37915c7f84861a37a37
parasol-tree <shilijingtian@outlook.com>
2023-06-03 01:57:17 +0800

    [doc] 修改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -85,7 +85,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   // base: process.env.BASE_URL,
   base: process.env.NODE_ENV === 'production' ? '/dew-ui/' : '/',
   routes
